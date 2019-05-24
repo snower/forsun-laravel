c7a76ddadfbfa89330157daee318d72473b2081b
snower <sujian199@gmail.com>
2019-05-24 18:36:20 +0800

    add support 5.5 5.6 5.7 5.8

--- src/Jobs/EventFireHandler.php
+++ src/Jobs/EventFireHandler.php
@@ -13,7 +13,18 @@ use Illuminate\Container\Container;
 
 class EventFireHandler implements ShouldQueue
 {
-    public function handle($event, $payload = [], $halt = false){
-        Container::getInstance()->make('events')->fire($event, $payload, $halt);
+    public $event;
+    public $payload;
+    public $halt;
+
+    public function __construct($event, $payload = [], $halt = false)
+    {
+        $this->event = $event;
+        $this->payload = $payload;
+        $this->halt = $halt;
+    }
+
+    public function handle(){
+        Container::getInstance()->make('events')->dispatch($this->event, $this->payload, $this->halt);
     }
 }
